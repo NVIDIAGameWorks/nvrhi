a042082d60225f30591fdf987d7d373498cb562c
Yassine Sen <61026912+01Pollux@users.noreply.github.com>
2023-12-22 13:17:13 +0100

    Add the missing `ICommandList::setStencilRefValue` method

--- include/nvrhi/nvrhi.h
+++ include/nvrhi/nvrhi.h
@@ -2536,6 +2536,7 @@ namespace nvrhi
         virtual void setPushConstants(const void* data, size_t byteSize) = 0;
 
         virtual void setGraphicsState(const GraphicsState& state) = 0;
+        virtual void setStencilRefValue(uint8_t value) = 0;
         virtual void draw(const DrawArguments& args) = 0;
         virtual void drawIndexed(const DrawArguments& args) = 0;
         virtual void drawIndirect(uint32_t offsetBytes, uint32_t drawCount = 1) = 0;
