4dc4f3a019a3508b490db789e8c58f3b536a15ff
Loopunit <1158984+loopunit@users.noreply.github.com>
2024-06-18 21:26:09 -0700

    Fixing static_vector memory leak in validatePipelineBindingLayouts
    DeviceWrapper::validatePipelineBindingLayouts will leak memory allocated for bindingsPerLayout & duplicatesPerLayout's members due to the way static_vector's resize is implemented, which assumes that all std::array's elements are in an uninitialized state by default.
    
    Changing static_vector to use assignment in resize instead of placement new/delete fixes the issue without adding a bunch of extra plumbing or running the destructor over each element in static_vector's constructor.

--- include/nvrhi/common/containers.h
+++ include/nvrhi/common/containers.h
@@ -138,12 +138,12 @@ struct static_vector : private std::array<T, _max_elements>
         if (current_size > new_size)
         {
             for (size_type i = new_size; i < current_size; i++)
-                (data() + i)->~T();
+                *(data() + i) = T{};
         }
         else
         {
             for (size_type i = current_size; i < new_size; i++)
-                new (data() + i) T();
+                *(data() + i) = T{};
         }
 
         current_size = new_size;
